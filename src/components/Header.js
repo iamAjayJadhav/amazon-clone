e9d933bacebd7c909a3ce66f37530e57b4b6f7c6
Ajay <ajayjadhavarj@gmail.com>
2021-08-24 18:17:14 +0530

    header section top navbar

---
+++ src/components/Header.js
@@ -0,0 +1,53 @@
+import Image from "next/image"
+import {
+  MenuIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/outline"
+function Header() {
+  return (
+    <header>
+      {/*Top Nav*/}
+      <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+        <div className="mt-2 flex items-center flex-grow flex-shrink sm:flex-grow-0">
+          <Image
+            src="https://links.papareact.com/f90"
+            width={150}
+            height={40}
+            objectFit="contain"
+            className="cursor-pointer"
+          />
+        </div>
+
+        {/* search bar */}
+        <div className="hidden sm:flex items-center h-10 rounded-md flex-grow cursor-pointer bg-yellow-400 hover:bg-yellow-500">
+          <input
+            className="p-2 h-full w-6 flex-grow rounded-l-md focus:outline-none px-4"
+            type="text"
+          />
+          <SearchIcon className="h-12 p-4" />
+        </div>
+
+        {/* right */}
+        <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
+          <div className="cursor-pointer">
+            <p>Hello Ajay</p>
+            <p>Accounts & Lists </p>
+          </div>
+          <div className="cursor-pointer">
+            <p>Returns</p>
+            <p>& Order</p>
+          </div>
+          <div className="cursor-pointer">
+            <ShoppingCartIcon className="h-18" />
+            <p>Basket</p>
+          </div>
+        </div>
+      </div>
+      {/*Bottom Nav*/}
+      <div></div>
+    </header>
+  )
+}
+
+export default Header
